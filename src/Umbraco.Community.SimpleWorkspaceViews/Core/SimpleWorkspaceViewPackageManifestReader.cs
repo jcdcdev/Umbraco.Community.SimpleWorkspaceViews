d6d5665e6431e0bea38108c792c00bb3f64780b5
jcdcdev <james@jcdc.dev>
2024-06-25 23:20:30 +0100

    refactor: move manifest models to core library

--- src/Umbraco.Community.SimpleWorkspaceViews/Core/SimpleWorkspaceViewPackageManifestReader.cs
+++ src/Umbraco.Community.SimpleWorkspaceViews/Core/SimpleWorkspaceViewPackageManifestReader.cs
@@ -67,23 +67,3 @@ public class SimpleWorkspaceViewPackageManifestReader(ISimpleWorkspaceViewServic
         return new[] { packageManifest };
     }
 }
-
-public class WorkspaceViewManifest : IManifest
-{
-    public string Alias { get; set; }
-    public string Name { get; set; }
-    public string Js { get; set; }
-    public int Weight { get; set; }
-    public MetaManifest Meta { get; set; }
-    public IEnumerable<ConditionManifest> Conditions { get; set; }
-    public string Element { get; set; }
-    public string ElementName { get; set; }
-    public string Type => "workspaceView";
-
-    public class MetaManifest
-    {
-        public string Icon { get; set; }
-        public string Label { get; set; }
-        public string Pathname { get; set; }
-    }
-}
