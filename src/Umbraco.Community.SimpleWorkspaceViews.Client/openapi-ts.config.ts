c48cc6c490bd125fcb748d0fe4be7fcfad1dd28b
jcdcdev <james@jcdc.dev>
2024-11-17 22:55:03 +0000

    chore: update front end dependencies

--- src/Umbraco.Community.SimpleWorkspaceViews.Client/openapi-ts.config.ts
+++ src/Umbraco.Community.SimpleWorkspaceViews.Client/openapi-ts.config.ts
@@ -1,8 +1,7 @@
-import {defineConfig} from '@hey-api/openapi-ts';
+import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-    input:
-        'http://localhost:54813/umbraco/swagger/SimpleWorkspaceViews/swagger.json',
+    input: 'http://localhost:54813/umbraco/swagger/SimpleWorkspaceViews/swagger.json',
     output: {
         format: 'prettier',
         path: './src/api',
